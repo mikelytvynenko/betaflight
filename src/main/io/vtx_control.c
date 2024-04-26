26b54f1040c3a72bbafae42414dd7ab79704f208
mike <mikelytvynenko@gmail.com>
2024-04-26 18:08:38 +0300

    remove ARM block on vtx

--- src/main/io/vtx_control.c
+++ src/main/io/vtx_control.c
@@ -55,7 +55,7 @@ PG_RESET_TEMPLATE(vtxConfig_t, vtxConfig,
     .halfDuplex = true
 );
 
-static uint8_t locked = 0;
+// static uint8_t locked = 0;
 
 
 void vtxControlInit(void)
@@ -74,11 +74,12 @@ void vtxControlInputPoll(void)
 
 static void vtxUpdateBandAndChannel(uint8_t bandStep, uint8_t channelStep)
 {
-    if (ARMING_FLAG(ARMED)) {
-        locked = 1;
-    }
+    // if (ARMING_FLAG(ARMED)) {
+    //     locked = 1;
+    // }
 
-    if (!locked && vtxCommonDevice()) {
+    if (//!locked && 
+        vtxCommonDevice()) {
         vtxSettingsConfigMutable()->band += bandStep;
         vtxSettingsConfigMutable()->channel += channelStep;
     }
@@ -106,9 +107,9 @@ void vtxDecrementChannel(void)
 
 void vtxUpdateActivatedChannel(void)
 {
-    if (ARMING_FLAG(ARMED)) {
-        locked = 1;
-    }
+    // if (ARMING_FLAG(ARMED)) {
+    //     locked = 1;
+    // }
 
     if (vtxCommonDevice()) {
         static uint8_t lastIndex = -1;
@@ -120,14 +121,14 @@ void vtxUpdateActivatedChannel(void)
                 && index != lastIndex) {
                 lastIndex = index;
 
-                if (!locked) {
+                // if (!locked) {
                     if (vtxChannelActivationCondition->band > 0) {
                         vtxSettingsConfigMutable()->band = vtxChannelActivationCondition->band;
                     }
                     if (vtxChannelActivationCondition->channel > 0) {
                         vtxSettingsConfigMutable()->channel = vtxChannelActivationCondition->channel;
                     }
-                }
+                // }
 
                 if (vtxChannelActivationCondition->power > 0) {
                     vtxSettingsConfigMutable()->power = vtxChannelActivationCondition->power;
